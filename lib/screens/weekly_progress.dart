9b024e269a5de69d0c671aa9070ef89288e40009
Trevor Owens <trevor@leanstartupmachine.com>
2020-06-28 13:46:15 -0400

    weekly progress screen

---
+++ lib/screens/weekly_progress.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class WeeklyProgress extends StatefulWidget {
+  @override
+  _WeeklyProgressState createState() => _WeeklyProgressState();
+}
+
+class _WeeklyProgressState extends State<WeeklyProgress> {
+  List<Widget> weeks = [
+    Card(
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+        child: Row(
+          children: <Widget>[
+            Text('Week 1', style: TextStyle(fontWeight: FontWeight.bold)),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Icon(Icons.check_circle, color: Colors.green),
+                  Icon(Icons.check_circle, color: Colors.green),
+                  Icon(Icons.check_circle, color: Colors.green),
+                  Icon(Icons.not_interested, color: Colors.grey[500]),
+                  Icon(Icons.check_circle, color: Colors.green),
+                  Icon(Icons.not_interested, color: Colors.grey[500]),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Your Progress"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: weeks),
+        ),
+      ),
+    );
+  }
+}
